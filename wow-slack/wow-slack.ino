16059a15e5db44eb5736accc19303d807f76a37d
Netopya <netopya@netopyaplanet.com>
2018-10-07 14:59:16 -0400

    Added in slack integration
    Bot now plays a sound on the wow message text

--- wow-slack/wow-slack.ino
+++ wow-slack/wow-slack.ino
@@ -62,35 +62,183 @@ https://open-smart.aliexpress.com/store/1199788
 #include <SoftwareSerial.h>
 #include "RedMP3.h"
 
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WebSocketsClient.h>
+
+#define SLACK_BOT_TOKEN "#"
+#define WIFI_SSID "#"
+#define WIFI_PASSWORD "#"
+
+// if Slack changes their SSL fingerprint, you would need to update this:
+#define SLACK_SSL_FINGERPRINT "C1 0D 53 49 D2 3E E5 2B A2 61 D5 9E 6F 99 0D 3D FD 8B B2 B3" 
+
+#define WORD_SEPERATORS "., \"'()[]<>;:-+&?!\n\t"
+
 #define MP3_RX 12//RX of Serial MP3 module connect to D7 of Arduino
 #define MP3_TX 13//TX to D8, note that D8 can not be used as RX on Mega2560, you should modify this if you donot use Arduino UNO
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
 MP3* mp3;
 
 int8_t indexfoo  = 0x01;//the first song in the TF card
 int8_t volume = 0x10;//0~0x1e (30 adjustable level)
+
+long nextCmdId = 1;
+bool connected = false;
+unsigned long lastPing = 0;
+
+StaticJsonBuffer<20480> jsonBuffer;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(2000);//Requires 500ms to wait for the MP3 module to initialize
-  digitalWrite(LED_BUILTIN, HIGH);  
+  updateStatus(0);
+  delay(3000);
+
+  updateStatus(1);
   mp3 = new MP3(MP3_RX, MP3_TX);
-  digitalWrite(LED_BUILTIN, LOW);  
   delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);  
-  mp3->playWithVolume(indexfoo,volume);
-  digitalWrite(LED_BUILTIN, LOW);  
-  delay(50);//you should wait for >=50ms between two commands
+
+  updateStatus(2);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println("Starting Up");
+ 
+  updateStatus(3);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  Serial.println("");
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  updateStatus(4);
 }
+
+void processSlackMessage(const char *message) {
+  char *nextWord = NULL;
+  Serial.println("processing message");
+
+  char *payload = strdup(message);
+
+  for (nextWord = strtok(payload, WORD_SEPERATORS); nextWord; nextWord = strtok(NULL, WORD_SEPERATORS)) {
+    if (strcasecmp(nextWord, "wow") == 0) {
+      indexfoo++;
+      digitalWrite(LED_BUILTIN, indexfoo % 2);  
+      mp3->playWithVolume(indexfoo,volume);
+      // WOW! 
+    }
+  }
+
+  free(payload);
+}
+
+void sendPing() {
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["type"] = "ping";
+  root["id"] = nextCmdId++;
+  String json;
+  root.printTo(json);
+  webSocket.sendTXT(json);
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t len) {
+  switch (type) {
+    case WStype_CONNECTED:
+      Serial.printf("[WebSocket] Connected to: %s\n", payload);
+      break;
+
+    case WStype_TEXT:
+      Serial.printf("[WebSocket] Message: %s\n", payload);
+
+      JsonObject& root = jsonBuffer.parseObject((char*)payload);
+      const char* messageType = root["type"];
+      Serial.printf("mesage type: %s\n", messageType); 
+
+      /*char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+      JsonObject& root = jsonBuffer.parseObject(json);
+      const char* sensor = root["sensor"];
+      Serial.println(sensor);*/
+
+      if (strcmp(messageType, "message") == 0) {
+        const char* messageContent = root["text"];
+        processSlackMessage(messageContent);
+      }
+      
+      // TODO: process message
+      break;
+  }
+}
+
+bool connectToSlack() {
+  updateStatus(3);
+  
+  // Step 1: Find WebSocket address via RTM API (https://api.slack.com/methods/rtm.start)
+  HTTPClient http;
+  http.begin("https://slack.com/api/rtm.connect?token=" SLACK_BOT_TOKEN, SLACK_SSL_FINGERPRINT);
+  int httpCode = http.GET();
+
+  updateStatus(4);
+  
+  if (httpCode != HTTP_CODE_OK) {
+    Serial.printf("HTTP GET failed with code %d\n", httpCode);
+    return false;
+  }
+
+  Serial.printf("HTTP GET Code %d\n", httpCode);
+
+  WiFiClient *client = http.getStreamPtr();
+  client->find("wss:\\/\\/");
+  String host = client->readStringUntil('\\');
+  String path = client->readStringUntil('"');
+  path.replace("\\/", "/");
+
+  updateStatus(5);
+  
+  // Step 2: Open WebSocket connection and register event handler
+  Serial.println("WebSocket Host=" + host + " Path=" + path);
+  webSocket.beginSSL(host, 443, path, "", "");
+  webSocket.onEvent(webSocketEvent);
+
+  updateStatus(6);
+  
+  return true;
+}
+
+void updateStatus(int led){
+  digitalWrite(LED_BUILTIN, led % 2);  
+}
+
 
 void loop()
 {
-  indexfoo++;
-  delay(3000);
-  digitalWrite(LED_BUILTIN, indexfoo % 2);  
-  mp3->playWithVolume(indexfoo,volume);
-}
+  webSocket.loop();
 
-/*********************************************************************************************************
-The end of file
-*********************************************************************************************************/
+  if (connected) {
+    // Send ping every 5 seconds, to keep the connection alive
+    if (millis() - lastPing > 5000) {
+      sendPing();
+      lastPing = millis();
+    }
+  } else {
+    // Try to connect / reconnect to slack
+    connected = connectToSlack();
+    if (!connected) {
+      delay(500);
+    }
+  }
+}
